f1fd1e7c3417e28531322dc6b748fb541083d9cd
Mark Reilly <mark@morsolutions.com>
2014-04-23 00:18:59 +0100

    Icon Update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 $( document ).on( "pageinit", function( event ) {
 	var panel_html = ''+
-'			<ul data-role="listview" data-inset="true">'+
+'<ul data-role="listview" data-inset="true">'+
     '<li><a href="index.html"> <img src="icons/home.png"/> Home</a></li>'+
 	'<li><a href="games.html"> <img src="games/SpaceInvader.jpg"/> Game On!</a></li>'+
 '<li><a href="events.html"> <img src="icons/Events.png"/> Events</a></li>'+
-	'<li><a href="news.html"> <img src="icons/news.png"/> News</a></li>'+
-'	<li><a href="about.html"> <img src="icons/about.png"/> About</a></li>'+
+	//'<li><a href="news.html"> <img src="icons/news.png"/> News</a></li>'+
+'<li><a href="about.html"> <img src="icons/about.png"/> About</a></li>'+
 '<li><a href="attractions.html"> <img src="icons/attractions.png"/> Tourist Attractions</a></li>'+
 '<li><a href="contact.html"> <img src="icons/contact.png"/> CONTACT</a></li>'+
 
